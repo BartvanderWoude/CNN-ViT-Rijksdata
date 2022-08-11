3913a55909b5f1f66fc03dbe4836386e534157be
Altripayo <37404586+Altripayo@users.noreply.github.com>
2022-08-11 20:22:14 +0200

    Delete library directory

--- library/local_vit_pytorch/__init__.py
+++
@@ -1,5 +0,0 @@
-from library.local_vit_pytorch.vit import ViT
-from library.local_vit_pytorch.simple_vit import SimpleViT
-
-from library.local_vit_pytorch.mae import MAE
-from library.local_vit_pytorch.dino import Dino
